6a2935fcb2510db433dcea975ce9d72662f9b2c6
Peter Ivanic <peterivanic89@gmail.com>
2021-12-01 14:29:48 +0100

    make IntroduceYourself

---
+++ week3/day1/Exercises/src/IntroduceYourself.java
@@ -0,0 +1,16 @@
+public class IntroduceYourself {
+    public static void main(String[] args) {
+        // Write a program that prints a few details to the terminal window about you
+        // It should print each detail to a new line:
+        //  - Your name
+        //  - Your age
+        //  - Your height in meters (as a decimal fraction)
+
+        System.out.println("Peter Ivanic");
+        System.out.println("31");
+        System.out.println("1.75");
+
+
+    }
+
+}
