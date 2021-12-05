024d338f023ed9ec2a95198fa72734be279ff238
Peter Ivanic <peterivanic89@gmail.com>
2021-12-05 14:46:27 +0100

    make change in DrawPyramid.java

--- week3/day2/src/DrawPyramid.java
+++ week3/day2/src/DrawPyramid.java
@@ -1,8 +1,7 @@
 import java.util.Scanner;
 
 public class DrawPyramid {
-    public static void main(String[] args) {
-        // Write a program that draws a pyramid like this:
+    // Write a program that draws a pyramid like this:
 //
 //
 //    *
@@ -11,23 +10,27 @@ public class DrawPyramid {
 // *******
 //
 // The pyramid should have as many lines as the number was
+    public static void drawTriangle(int lineCount) {
+        for (int i = 1; i <= lineCount; i++) {
+            String s = "*";
+            String m = " ";
+            for (int j = i; j < lineCount; j++) {
+                System.out.print(m);
+            }
+            for (int k = 1; k < i * 2; k++) {
+                System.out.print(s);
+            }
+            System.out.println();
+        }
+    }
+
+
+    public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please write the count of line:");
         int line = scanner.nextInt();
         drawTriangle(line);
     }
 
-    public static void drawTriangle(int lineCount){
-        for (int i = 1; i <= lineCount; i++){
-            String s = "*";
-            String m = " " ;
-            for (int j = i; j < lineCount; j++){
-                System.out.print(m);
-            }
-            for (int k = 1; k < i*2; k++){
-                System.out.print(s);
-            }
-            System.out.println();
-        }
-    }
 }
