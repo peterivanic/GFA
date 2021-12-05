801e75817df757ccf2f4ee0743cc7a2b2996a80c
Peter Ivanic <peterivanic89@gmail.com>
2021-12-05 14:37:16 +0100

    make DrawPyramid.java

--- week3/day2/src/DrawPyramid.java
+++ week3/day2/src/DrawPyramid.java
@@ -18,13 +18,13 @@ public class DrawPyramid {
     }
 
     public static void drawTriangle(int lineCount){
-        for (int i = 0; i < lineCount; i++){
-            String s = "* ";
-            String m = " ";
-            for (int j = 0; j < lineCount - i; j++){
+        for (int i = 1; i <= lineCount; i++){
+            String s = "*";
+            String m = " " ;
+            for (int j = i; j < lineCount; j++){
                 System.out.print(m);
             }
-            for (int k = 0; k <= i; k++){
+            for (int k = 1; k < i*2; k++){
                 System.out.print(s);
             }
             System.out.println();
