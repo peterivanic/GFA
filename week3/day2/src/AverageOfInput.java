4cccebb247c1e728f0c7abc7554c58e0fa577879
Peter Ivanic <peterivanic89@gmail.com>
2021-12-12 13:30:24 +0100

    i  corrected AverageOfInput.java

--- week3/day2/src/AverageOfInput.java
+++ week3/day2/src/AverageOfInput.java
@@ -4,19 +4,19 @@ public class AverageOfInput {
     public static void main(String[] args) {
         // Write a program that asks for 5 integers in a row,
         // then it should print the sum and the average of these numbers like:
-
+        int sum = 0;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please write the 5 numbers:");
 
-        int numb1 = scanner.nextInt();
-        int numb2 = scanner.nextInt();
-        int numb3 = scanner.nextInt();
-        int numb4 = scanner.nextInt();
-        int numb5 = scanner.nextInt();
+       for (int i = 0; i < 5; i++){
+           int number = scanner.nextInt();
+           sum += number;
+       }
 
-        int sum = numb1 + numb2 + numb3 + numb4 + numb5;
-        int average = sum / 5;
 
-        System.out.println("Sum: " + sum + ", Average: " + average);
+
+        double average = (double) sum / 5;
+
+        System.out.println("Sum: " + sum + ", Average: " +  average);
     }
 }
