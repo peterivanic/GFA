04bac9331fb70eae938205ec93f7fe8a96bfa9bb
Peter Ivanic <peterivanic89@gmail.com>
2021-12-12 14:54:52 +0100

    make DiagonalMatrix.java

---
+++ week4/day1/Exercises/src/DiagonalMatrix.java
@@ -0,0 +1,37 @@
+public class DiagonalMatrix {
+    // - Create a two dimensional array dynamically with the following content.
+    //   Note that a two dimensional array is often called matrix if every
+    //   internal array has the same length.
+    //   Use a loop!
+    //
+    //   1 0 0 0
+    //   0 1 0 0
+    //   0 0 1 0
+    //   0 0 0 1
+    //
+    //   Its length should depend on a variable
+    //
+    // - Print this two dimensional array to the output
+    public static void main(String[] args) {
+        int length = 4;
+        int[][] matrix = new int[length][length];
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                if (i == j) {
+                    matrix[i][j] = 1;
+                }else {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        for (int[] i : matrix) {
+            for (int j = 0; j < matrix.length; j++) {
+                System.out.print(i[j]);
+            }
+            System.out.println();
+        }
+
+    }
+}
