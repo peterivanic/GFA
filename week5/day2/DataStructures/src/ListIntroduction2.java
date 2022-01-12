3fbd51350b042c9b198facd5b71b41709399d89e
Peter Ivanic <peterivanic89@gmail.com>
2022-01-12 17:03:56 +0100

    make ListIntroduction2.java

--- week5/day2/DataStructures/src/ListIntroduction2.java
+++ week5/day2/DataStructures/src/ListIntroduction2.java
@@ -1,19 +1,25 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
 
 public class ListIntroduction2 {
     public static void main(String[] args) {
         String[] list ={"Apple", "Avocado", "Blueberries", "Durian", "Lychee"};
-        List<String> listA;
-        List<String> listB = new ArrayList<>();
-        listA = List.of(list);
-        listB = listA;
+        ArrayList<String> listA = new ArrayList<>(Arrays.asList(list));
+        ArrayList<String> listB = new ArrayList<>(listA);
+
         System.out.println(listA.contains("Durian"));
-
-
-        listA.add("Kiwifruit");
         listB.remove("Durian");
-
+        listA.add(4,"Kiwifruit");
+        System.out.println(listA.size()<listB.size());
+        System.out.println(listA.indexOf("Avocado"));
+        System.out.println(listB.indexOf("Durian"));
+        String[] pp = {"Passion Fruit","Pomelo"};
+        listB.addAll(List.of(pp));
+        System.out.println(listA.get(2));
+        System.out.println(listA);
+        System.out.println(listB);
 
     }
 }
