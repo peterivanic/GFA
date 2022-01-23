c64bc29fcb33930176e2bbb9ca6fb581c2663216
Peter Ivanic <peterivanic89@gmail.com>
2022-01-23 11:27:20 +0100

    make SharpieSet.java

--- week8/day2/ObjectOrientedProgramming/src/Sharpie.java
+++ week8/day2/ObjectOrientedProgramming/src/Sharpie.java
@@ -15,12 +15,25 @@ public class Sharpie {
     public static void main(String[] args) {
         Sharpie firstSharpie = new Sharpie("brown", 30);
         Sharpie secondtSharpie = new Sharpie("red", 50);
+        Sharpie thirdSharpie = new Sharpie("black", 20);
 
         for (int i = 0; i < 15; i++) {
             firstSharpie.use();
             secondtSharpie.use();
         }
 
-        System.out.println(firstSharpie.inkAmount + " " + secondtSharpie.inkAmount);
+        SharpieSet sharpieSet = new SharpieSet();
+        sharpieSet.add(firstSharpie);
+        sharpieSet.add(secondtSharpie);
+        sharpieSet.add(thirdSharpie);
+
+        System.out.println(sharpieSet.countUsable());
+
+        for (int i = 0; i < 100; i++) {
+            firstSharpie.use();
+        }
+
+        sharpieSet.removeTrash();
+        System.out.println(sharpieSet.countUsable());
     }
 }
