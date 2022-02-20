37d7952e376c471a9888e640a0efc80f289a6ef3
Peter Ivanic <peterivanic89@gmail.com>
2022-02-20 17:01:52 +0100

    Make Todo.java app

---
+++ week11/day2/Todo/src/Tasks.java
@@ -0,0 +1,70 @@
+import javax.sound.sampled.Line;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Tasks {
+
+
+    Path path = Paths.get("C:\\GreenFox\\greenfox\\peterivanic\\week11\\day2\\Todo\\src\\tasks.txt");
+    FileWriter txt = new FileWriter(String.valueOf(path), true);
+
+
+    public Tasks() throws IOException {
+    }
+
+
+    public void addTask(String task) throws IOException {
+        List<String> lines = Files.readAllLines(path);
+
+        txt.write(lines.size()+1 + " - [ ] " + task + "\n");
+        txt.close();
+
+    }
+
+    public String listOfTask() throws IOException {
+        List<String> lines = Files.readAllLines(path);
+        if (lines.size()==0){
+            return "No todos for today! :)";
+        }else {
+           StringBuilder sb = new StringBuilder();
+            for (String line : lines) {
+                sb.append(line).append("\n");
+            }
+            return sb.toString();
+        }
+    }
+
+    public void removeTask(int indexOfDeleting) throws IOException {
+
+        List<String> lines = Files.readAllLines(path);
+        if (indexOfDeleting>lines.size()){
+            System.err.println("Unable to remove: index is out of bound");
+        }else {
+            lines.remove(indexOfDeleting-1);
+        }
+        Files.write(path,lines);
+    }
+
+    public void checkTask(int indexOfCheck) throws IOException {
+
+        List<String> lines = Files.readAllLines(path);
+        if (indexOfCheck>lines.size()){
+            System.err.println("Unable to remove: index is out of bound");
+        }else {
+        String newS = lines.get(indexOfCheck-1).replace("[ ]","[x]");
+        lines.remove(indexOfCheck-1);
+        lines.add(indexOfCheck-1,newS);
+        }
+        Files.write(path,lines);
+    }
+
+}
---
+++ week11/day2/Todo/src/Todo.java
@@ -0,0 +1,66 @@
+import javax.management.ObjectInstance;
+import java.io.IOException;
+import java.util.InputMismatchException;
+
+public class Todo {
+
+    public static void main(String[] args) throws IOException {
+        Tasks t = new Tasks();
+
+        if (args.length >= 1) {
+            if (args[0].equals("-l")) {
+                System.out.println(t.listOfTask());
+            }
+
+            if (args[0].equals("-a")) {
+                if (args.length < 2) {
+                    System.out.println("Unable to add: no task provided");
+                } else {
+                    var task = args[1];
+                    t.addTask(task);
+                }
+            }
+
+            if (args[0].equals("-r")) {
+                if (args.length < 2) {
+                    System.out.println("Unable to remove: no index provided");
+                } else {
+                    try {
+                        int index = Integer.parseInt(args[1]);
+                        t.removeTask(index);
+                    } catch (NumberFormatException | InputMismatchException e) {
+                        System.err.println("Unable to remove: index is not a number");
+                    }
+                }
+            }
+
+            if (args[0].equals("-c")){
+                if (args.length < 2) {
+                    System.out.println("Unable to check: no index provided");
+                } else {
+                    try {
+                        int index = Integer.parseInt(args[1]);
+                        t.checkTask(index);
+                    } catch (NumberFormatException | InputMismatchException e) {
+                        System.err.println("Unable to check: index is not a number");
+                    }
+                }
+            }
+
+
+        } else {
+            System.out.println("Command Line Todo application\n" +
+                    "=============================");
+            System.out.println();
+            System.out.println("Command line arguments:");
+            System.out.println(
+                    """
+                                -l   Lists all the tasks
+                                -a   Adds a new task
+                                -r   Removes an task
+                                -c   Completes an task
+                            """);
+        }
+
+    }
+}
