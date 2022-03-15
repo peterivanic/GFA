7b54b43637d4479bdda58dad1d940994c4793a12
Peter Ivanic <peterivanic89@gmail.com>
2022-03-15 21:48:29 +0100

    make evenNumbers method in StreamUtils.java

---
+++ week14/day1/streams/src/StreamUtils.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class StreamUtils {
+
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14);
+        System.out.println(evenNumbers(numbers));
+    }
+
+    public static List<Integer> evenNumbers(List<Integer> list) {
+        return list.stream()
+                .filter(x -> x % 2 == 0)
+                .toList();
+    }
+}
