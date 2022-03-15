d912c89993807bdc30fc279d6f56ffb9fc93a2c2
Peter Ivanic <peterivanic89@gmail.com>
2022-03-15 22:01:35 +0100

    make squaresOfPositiveNumbers method in StreamUtils.java

--- week14/day1/streams/src/StreamUtils.java
+++ week14/day1/streams/src/StreamUtils.java
@@ -6,11 +6,19 @@ public class StreamUtils {
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14);
         System.out.println(evenNumbers(numbers));
+        System.out.println(squaresOfPositiveNumbers(numbers));
     }
 
     public static List<Integer> evenNumbers(List<Integer> list) {
         return list.stream()
                 .filter(x -> x % 2 == 0)
+                .toList();
+    }
+
+    public static List<Integer> squaresOfPositiveNumbers(List<Integer> list) {
+        return list.stream()
+                .filter(x -> x > 0)
+                .map(x -> x * x)
                 .toList();
     }
 }
