309abd3a1b5eba115571fbd0abae22f0672e6916
Peter Ivanic <peterivanic89@gmail.com>
2022-03-16 12:23:11 +0100

    make squaresIsGreater method in StreamUtils.java

--- week14/day1/streams/src/StreamUtils.java
+++ week14/day1/streams/src/StreamUtils.java
@@ -1,12 +1,16 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.OptionalDouble;
 
 public class StreamUtils {
 
     public static void main(String[] args) {
-        List<Integer> numbers = Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14);
-        System.out.println(evenNumbers(numbers));
-        System.out.println(squaresOfPositiveNumbers(numbers));
+        List<Integer> numbers1 = Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14);
+        System.out.println(evenNumbers(numbers1));
+        System.out.println(squaresOfPositiveNumbers(numbers1));
+        List<Integer> numbers2 = Arrays.asList(3, 9, 2, 8, 6, 5);
+        System.out.println(squaresIsGreater(numbers2,20));
+
     }
 
     public static List<Integer> evenNumbers(List<Integer> list) {
@@ -21,4 +25,11 @@ public class StreamUtils {
                 .map(x -> x * x)
                 .toList();
     }
+
+    public static List<Integer> squaresIsGreater(List<Integer> listOfNumbers, int greaterThan) {
+        return listOfNumbers.stream()
+                .filter(x -> x * x > greaterThan)
+                .toList();
+    }
+
 }
