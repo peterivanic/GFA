1b4daaa32805434506281a82eb2857894dfcefdc
Peter Ivanic <peterivanic89@gmail.com>
2022-03-22 21:45:30 +0100

    made hello method

--- Week 15 - Spring intro, Thymeleaf, Models, Views, Basic webshop/day1/src/main/java/com/greenfoxacademy/springstart1/Springstart1Application.java
+++ Week 15 - Spring intro, Thymeleaf, Models, Views, Basic webshop/day1/src/main/java/com/greenfoxacademy/springstart1/Springstart1Application.java
@@ -2,12 +2,21 @@ package com.greenfoxacademy.springstart1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
+@Controller
 public class Springstart1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Springstart1Application.class, args);
 	}
 
+	@RequestMapping(value="/hello")
+	@ResponseBody
+	public String hello(){
+		return "Hello world!";
+	}
 }
