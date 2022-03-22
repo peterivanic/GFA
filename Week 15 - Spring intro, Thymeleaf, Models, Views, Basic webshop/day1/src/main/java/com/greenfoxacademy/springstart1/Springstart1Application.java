b4d034428749e30e75e52ecffb1b40f15ba322d3
Peter Ivanic <peterivanic89@gmail.com>
2022-03-22 21:37:37 +0100

    made springstart1 project

---
+++ Week 15 - Spring intro, Thymeleaf, Models, Views, Basic webshop/day1/src/main/java/com/greenfoxacademy/springstart1/Springstart1Application.java
@@ -0,0 +1,13 @@
+package com.greenfoxacademy.springstart1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springstart1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springstart1Application.class, args);
+	}
+
+}
---
+++ Week 15 - Spring intro, Thymeleaf, Models, Views, Basic webshop/day1/src/test/java/com/greenfoxacademy/springstart1/Springstart1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenfoxacademy.springstart1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springstart1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
