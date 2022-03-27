c0a3b6dc9871de11ecba40f6a41459e17be9ddbe
Peter Ivanic <peterivanic89@gmail.com>
2022-03-27 14:25:54 +0200

    made sayhello app

---
+++ Week 15 - Spring intro, Thymeleaf, Models, Views, Basic webshop/day2/src/main/java/com/greenfox/sayhello/controllers/SayHelloController.java
@@ -0,0 +1,23 @@
+package com.greenfox.sayhello.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SayHelloController {
+    @RequestMapping("/hello")
+    public String hello(Model model){
+
+        String[] hellos = {"Mirëdita", "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý den", "Ahoj", "Goddag", "Goede dag, Hallo", "Hello", "Saluton", "Hei", "Bonjour",
+                "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Namaste", "Jó napot", "Halló", "Helló", "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit",
+                "Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao", "Dzien' dobry", "Olá", "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej",
+                "Sa-wat-dee", "Merhaba", "Selam", "Vitayu", "Xin chào", "Hylo", "Sut Mae", "Sholem Aleychem", "Sawubona"};
+
+        int i = (int) (Math.random()* hellos.length -1);
+        String word = hellos[i];
+        model.addAttribute("word",word);
+
+        return "hello";
+    }
+}
