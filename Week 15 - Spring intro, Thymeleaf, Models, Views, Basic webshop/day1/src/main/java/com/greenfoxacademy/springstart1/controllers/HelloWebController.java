459ff01695db498508340e0f7e95401d683629c6
Peter Ivanic <peterivanic89@gmail.com>
2022-03-27 13:37:49 +0200

    made greetweb

--- Week 15 - Spring intro, Thymeleaf, Models, Views, Basic webshop/day1/src/main/java/com/greenfoxacademy/springstart1/controllers/HelloWebController.java
+++ Week 15 - Spring intro, Thymeleaf, Models, Views, Basic webshop/day1/src/main/java/com/greenfoxacademy/springstart1/controllers/HelloWebController.java
@@ -1,8 +1,10 @@
 package com.greenfoxacademy.springstart1.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 public class HelloWebController {
 
     @RequestMapping("/web/greeting")
