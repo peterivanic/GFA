10715fdabe01fb5d14bf205513016c7565d800fc
Peter Ivanic <peterivanic89@gmail.com>
2022-04-10 13:05:53 +0200

    add greenfox.png

--- Week 17 - Dependencies, Programmer Fox Club/Programmer Fox Club/src/main/java/com/greenfox/foxclubapp/controller/MainController.java
+++ Week 17 - Dependencies, Programmer Fox Club/Programmer Fox Club/src/main/java/com/greenfox/foxclubapp/controller/MainController.java
@@ -1,4 +1,20 @@
 package com.greenfox.foxclubapp.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class MainController {
+
+    @GetMapping("/")
+    public String getIndex (){
+        return "index";
+    }
+
+    @GetMapping("/login")
+    public String getLogin(){
+        return "login";
+    }
+
+
 }
