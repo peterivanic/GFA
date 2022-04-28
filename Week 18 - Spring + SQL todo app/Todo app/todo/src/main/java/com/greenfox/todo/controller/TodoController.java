32f2e39077bd8acdd9cc9a2830349fcb6bcc486f
Peter Ivanic <peterivanic89@gmail.com>
2022-04-28 21:28:14 +0200

    made add new todo endpoint

--- Week 18 - Spring + SQL todo app/Todo app/todo/src/main/java/com/greenfox/todo/controller/TodoController.java
+++ Week 18 - Spring + SQL todo app/Todo app/todo/src/main/java/com/greenfox/todo/controller/TodoController.java
@@ -1,13 +1,12 @@
 package com.greenfox.todo.controller;
 
+import com.greenfox.todo.model.Todo;
 import com.greenfox.todo.repositories.TodoRepository;
 import com.greenfox.todo.service.TodoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.stream.Collectors;
 
@@ -27,5 +26,18 @@ public class TodoController {
                         .collect(Collectors.toList())
                         );
         return "todolist";
+    }
+
+    @GetMapping("/addTodo")
+    public String addNew(){
+        return "addTodo";
+    }
+
+    @PostMapping("/addTodo")
+    public String handleData(@RequestParam("text") String text){
+        Todo todo = new Todo();
+        todo.setTitle(text);
+        todoRepository.save(todo);
+        return "redirect:/todo";
     }
 }
